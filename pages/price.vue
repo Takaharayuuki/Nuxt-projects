845948fb66df952ef7dab3f2a4bf17e3b44e55ee
Takaharayuuki <takahara.yuuki22@gmail.com>
2021-01-07 10:41:38 +0900

    feat: Nuxtでのルーティング理解

---
+++ pages/price.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+    <h1>Price page</h1>
+    <router-link to="/">Top Page</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
